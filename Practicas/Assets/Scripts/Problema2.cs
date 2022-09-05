1b7d5ffb4366851db7ff501ea277b44ba82e833d
Humberto Trujillo <htp.tito@gmail.com>
2022-09-05 16:47:36 -0700

    Se completo problema

--- Practicas/Assets/Scripts/Problema2.cs
+++ Practicas/Assets/Scripts/Problema2.cs
@@ -4,15 +4,33 @@ using UnityEngine;
 
 public class Problema2 : MonoBehaviour
 {
+    public int[] nums = new int[4] {2,7,11,15};
+    public int target = 18;
+    public int[] expectedResult = new int[2] {0, 1};
+
     // Start is called before the first frame update
     void Start()
     {
-        
+        Debug.Log("Problema 2");
+        int[] result = SumaDos(nums, target);
+        Debug.Log(string.Join(", ", result));
+        Debug.Log($"Paso prueba: {TestSumaDos()}");
     }
 
-    // Update is called once per frame
-    void Update()
+    public int[] SumaDos(int[] nums, int target)
     {
-        
+        int[] result = new int[2] {0, 1};
+        return result;
+    }
+
+    public bool TestSumaDos()
+    {
+        int[] actualResult = SumaDos(nums, target);
+        if (actualResult.Length != expectedResult.Length) return false;
+        for (int i = 0; i < actualResult.Length; i++)
+        {
+            if (actualResult[i] != expectedResult[i]) return false;
+        }
+        return true;
     }
 }
